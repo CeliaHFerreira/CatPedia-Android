ebcf9d73401c68d14a8b2a15c9ab2864fa100fca
Celia <celiahrrferreira@gmail.com>
2022-05-11 00:09:06 +0200

    DEVELOP: Added navigation bottom bar, change some colors and refactor app with different fragments
    colors not defined yet

--- app/src/main/java/com/celia/catpedia_android/HomeActivity.kt
+++ app/src/main/java/com/celia/catpedia_android/HomeActivity.kt
@@ -4,9 +4,14 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.celia.catpedia_android.databinding.ActivityHomeBinding
+import com.celia.catpedia_android.fragments.BreedsFragment
+import com.celia.catpedia_android.fragments.FavoritesFragment
 import com.celia.catpedia_android.models.Breed
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
@@ -25,8 +30,15 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.navigation.setOnNavigationItemSelectedListener {
+            selectFragment(it)
+            true
+        }
+
+        binding.navigation.selectedItemId = R.id.navigation_home
 
         val bundle = intent.extras
         val email = bundle?.getString("email")
@@ -37,7 +49,8 @@ class HomeActivity : AppCompatActivity() {
         preferences.putString("email", email)
         preferences.apply()
 
-        getBreeds()
+        //getBreeds()
+
     }
 
     private fun setup() {
@@ -48,37 +61,54 @@ class HomeActivity : AppCompatActivity() {
         }
     }
 
-    private fun getRetrofit(): Retrofit {
+    private fun selectFragment(it: MenuItem) {
+
+        val fragmentClicked: Fragment
+
+        when (it.itemId) {
+            R.id.navigation_home -> fragmentClicked = BreedsFragment.newInstance()
+            else -> fragmentClicked = FavoritesFragment.newInstance()
+        }
+
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.homeContent, fragmentClicked)
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+        ft.commit()
+
+    }
+
+
+    /*private fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://api.thecatapi.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-    }
+    }*/
 
-    private fun getBreeds() {
-        CoroutineScope(Dispatchers.IO).launch {
-            val call = getRetrofit().create(APIService::class.java)
-                .getCatsBreeds("v1/breeds")
-                .execute()
-            val breeds = call.body()
-            runOnUiThread {
-                if (call.isSuccessful) {
-                    breeds?.let { showBreeds(it) }
-                } else {
-                    showError()
-                }
-            }
-        }
-    }
+    /* private fun getBreeds() {
+         CoroutineScope(Dispatchers.IO).launch {
+             val call = getRetrofit().create(APIService::class.java)
+                 .getCatsBreeds("v1/breeds")
+                 .execute()
+             val breeds = call.body()
+             runOnUiThread {
+                 if (call.isSuccessful) {
+                     breeds?.let { showBreeds(it) }
+                 } else {
+                     showError()
+                 }
+             }
+         }
+     }*/
 
-    private fun showBreeds(breeds: List<Breed>) {
+    /*private fun showBreeds(breeds: List<Breed>) {
         breedList.clear()
         breedList.addAll(breeds)
         adapter = BreedsAdapter(breedList)
         binding.rvBreeds.setHasFixedSize(true)
         binding.rvBreeds.layoutManager = LinearLayoutManager(this)
         binding.rvBreeds.adapter = adapter
-    }
+    }*/
 
     private fun returnToLogin() {
         val preferences = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
@@ -88,9 +118,10 @@ class HomeActivity : AppCompatActivity() {
         startActivity(loginIntent)
     }
 
-    private fun showError() {
+    public fun showError() {
         Toast.makeText(this, "Ha ocurrido un error", Toast.LENGTH_SHORT)
             .show()
     }
 }
+
 
---
+++ app/src/main/java/com/celia/catpedia_android/fragments/BreedsFragment.kt
@@ -0,0 +1,80 @@
+package com.celia.catpedia_android.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.celia.catpedia_android.APIService
+import com.celia.catpedia_android.BreedsAdapter
+import com.celia.catpedia_android.databinding.FragmentHomeListBinding
+import com.celia.catpedia_android.models.Breed
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class BreedsFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeListBinding
+    private lateinit var adapter: BreedsAdapter
+    private val breedList = mutableListOf<Breed>()
+
+
+    companion object {
+        fun newInstance(): BreedsFragment {
+            return BreedsFragment()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val breeds = getBreeds()
+            breedList.addAll(breeds)
+            binding.rvBreeds.layoutManager = LinearLayoutManager(activity)
+            binding.rvBreeds.adapter  = BreedsAdapter(breedList)
+        }
+    }
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.thecatapi.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private suspend fun getBreeds(): List<Breed> {
+        val breeds: List<Breed>
+        withContext(Dispatchers.IO) {
+            val call = getRetrofit().create(APIService::class.java)
+                .getCatsBreeds("v1/breeds")
+                .execute()
+            breeds = call.body()!!
+            if (!call.isSuccessful) {
+                showError()
+            }
+        }
+        return breeds
+    }
+
+    private fun showError() {
+        Toast.makeText(activity, "Ha ocurrido un error", Toast.LENGTH_SHORT)
+            .show()
+    }
+
+}
